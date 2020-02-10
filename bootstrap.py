759aeb5835dfa95b3a387027b5f3c4bab2d5bf31
Sergey Kosarevsky <sk@linderdaum.com>
2020-02-11 02:48:14 +0300

    Ch1: initial commit

---
+++ bootstrap.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import os
+import sys
+import platform
+import subprocess
+
+Python = "python" if platform.system() == "Windows" else "python3"
+
+def executeCommandSilent(command):
+	out = open(os.devnull, 'w')
+	err = subprocess.STDOUT
+	return subprocess.call(command, shell = True, stdout=out, stderr=err);
+
+def runPythonScript(Script, Params):
+	if os.system( Python + " " + Script + " " + Params ) != 0:
+		print( "Unable to run " + Script )
+		exit(255)
+
+if executeCommandSilent("python --version") != 0:
+	print( "Make sure Python can be started from the command line (add path to `python.exe` to PATH on Windows)" )
+	exit(255)
+
+if executeCommandSilent("cmake --version") != 0:
+	print( "Install CMake first" )
+	exit(255)
+
+if executeCommandSilent("git --version") != 0:
+	print( "Install Git first" )
+	exit(255)
+
+if executeCommandSilent("hg --version") != 0:
+	print( "Install Mercurial first" )
+	exit(255)
+
+# run Tools/Bootstrap
+
+runPythonScript( os.path.join("deps", "bootstrap.py"), "-b deps" )
+
+print( "" )
+print( "Bootstrapping done." )
