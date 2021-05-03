f6377fcbb7bf68df47bb4cd2e4880a7a15ead1d4
Sergey Kosarevsky <sk@linderdaum.com>
2021-05-04 01:48:46 +0300

    Ch2: updated Taskflow to v3.1.0

--- Chapter2/09_Taskflow/src/main.cpp
+++ Chapter2/09_Taskflow/src/main.cpp
@@ -14,23 +14,23 @@ int main()
 
 	std::vector<int> items{ 1, 2, 3, 4, 5, 6, 7, 8 };
 
-	auto [s, t] = taskflow.parallel_for(
-		items.begin(), items.end(), []( int item )
+	auto task = taskflow.for_each(
+		items.begin(), items.end(), [](int item)
 		{
-			std::cout << std::this_thread::get_id() << " runs " << item << std::endl;
+			std::cout << item;
 		}
 	);
 
-	s.work( []() { std::cout << "S - Start\n"; } ).name( "S" );
-	t.work( []() { std::cout << "T - End\n"; } ).name( "T" );
+	taskflow.emplace([]() { std::cout << "\nS - Start\n"; }).name("S").precede(task);
+	taskflow.emplace([]() { std::cout << "\nT - End\n"; }).name("T").succeed(task);
 
 	{
-		std::ofstream os( "taskflow.dot" );
-		taskflow.dump( os );
+		std::ofstream os("taskflow.dot");
+		taskflow.dump(os);
 	}
 
 	tf::Executor executor;
-	executor.run( taskflow ).wait();
+	executor.run(taskflow).wait();
 
 	return 0;
 }
