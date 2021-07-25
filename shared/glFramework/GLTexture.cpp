ccdac7451d10b0804fcb81d092afac84992fcb15
Sergey Kosarevsky <sk@linderdaum.com>
2021-07-25 22:56:48 +0300

    Added safeguards for missing lazy-loaded GL and Vulkan textures

--- shared/glFramework/GLTexture.cpp
+++ shared/glFramework/GLTexture.cpp
@@ -30,35 +30,32 @@ GLTexture::GLTexture(GLenum type, int width, int height, GLenum internalFormat)
 	glTextureStorage2D(handle_, getNumMipMapLevels2D(width, height), internalFormat, width, height);
 }
 
-namespace
+/// Draw a checkerboard on a pre-allocated square RGB image.
+uint8_t* genDefaultCheckerboardImage(int* width, int* height)
 {
-	/// Draw a checkerboard on a pre-allocated square RGB image.
-	uint8_t* genDefaultCheckerboardImage(int* width, int* height)
+	const int w = 128;
+	const int h = 128;
+
+	uint8_t* imgData = (uint8_t*)malloc(w * h * 3); // stbi_load() uses malloc(), so this is safe
+
+	assert(imgData && w > 0 && h > 0);
+	assert(w == h);
+
+	if (!imgData || w <= 0 || h <= 0) return nullptr;
+	if (w != h) return nullptr;
+
+	for (int i = 0; i < w * h; i++)
 	{
-		const int w = 128;
-		const int h = 128;
-
-		uint8_t* imgData = (uint8_t*)malloc(w * h * 3); // stbi_load() uses malloc(), so this is safe
-
-		assert(imgData && w > 0 && h > 0);
-		assert(w == h);
-
-		if (!imgData || w <= 0 || h <= 0) return nullptr;
-		if (w != h) return nullptr;
-
-		for (int i = 0; i < w * h; i++)
-		{
-			const int row = i / w;
-			const int col = i % w;
-			imgData[i * 3 + 0] = imgData[i * 3 + 1] = imgData[i * 3 + 2] = 0xFF * ((row + col) % 2);
-		}
-
-		if (width) *width = w;
-		if (height) *height = h;
-
-		return imgData;
+		const int row = i / w;
+		const int col = i % w;
+		imgData[i * 3 + 0] = imgData[i * 3 + 1] = imgData[i * 3 + 2] = 0xFF * ((row + col) % 2);
 	}
-} // namespace
+
+	if (width) *width = w;
+	if (height) *height = h;
+
+	return imgData;
+}
 
 GLTexture::GLTexture(GLenum type, const char* fileName)
 	: type_(type)
